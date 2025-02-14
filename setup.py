62187b26eba10b3c5ac3d8f65d230bf4d16d881f
kiannaquines <kjgnaquines@gmail.com>
2025-02-14 21:33:25 +0800

    Initialize project structure with setup configuration and essential files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="httpstatus",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests", 
+    ],
+    description="A Python package for handling HTTP status",
+    author="Kian Naquines",
+    author_email="kjgnaquines@gmail.com",
+    url="https://github.com/kiannaquines/httpstatus",
+)
