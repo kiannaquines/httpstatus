29d1464d52bc0b940e8debe03c50df8c299e54d5
kiannaquines <kjgnaquines@gmail.com>
2025-02-14 21:46:30 +0800

    Update package description in setup.py for clarity

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[],
-    description="A Python package for handling HTTP status",
+    description="A Python package providing an enumeration of HTTP status codes with names and descriptions",
     author="Kian Naquines",
     author_email="kjgnaquines@gmail.com",
     url="https://github.com/kiannaquines/httpstatus",
