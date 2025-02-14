b8ea5bbf3916d588213bf6fd1933e920ceaad857
kiannaquines <kjgnaquines@gmail.com>
2025-02-14 21:35:22 +0800

    Remove unnecessary dependencies from setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ setup(
     name="httpstatus",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        "requests", 
-    ],
+    install_requires=[],
     description="A Python package for handling HTTP status",
     author="Kian Naquines",
     author_email="kjgnaquines@gmail.com",
