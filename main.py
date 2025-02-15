1ccdcc5165ebfabfaf2371ba86e7b53736201e6e
kiannaquines <kjgnaquines@gmail.com>
2025-02-15 21:12:43 +0800

    Format print statements for HTTP status codes in main.py for improved readability

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from httpstatus.http_status import HTTPStatus
 print(HTTPStatus.OK)
 
 status = HTTPStatus.NOT_FOUND
-print(status.status)       # Output: 404
-print(status.name)         # Output: Not Found
-print(status.description)  # Output: The requested resource could not be found but may be available in the future.
+print(status.status)
+print(status.name)         
+print(status.description)
 
 success_codes = HTTPStatus.success_responses()
-print(success_codes)  # Output: List of all 2xx success status codes
+print(success_codes)
 
 status = HTTPStatus.from_status(500)
-print(status)  # Output: 500 Internal Server Error: A generic error message, given when an unexpected condition was encountered.
+print(status)
